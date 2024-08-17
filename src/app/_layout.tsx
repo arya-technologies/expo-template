b6d07e38692092d39244d95e0fc71f5ddb5411c2
MRINMOY MONDAL <mm2922894@gmail.com>
2024-08-18 02:25:20 +0530

    added gesture handler

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,17 +3,16 @@ import { persistor, store } from "@/features/store";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import * as NavigationBar from "expo-navigation-bar";
 import { Stack } from "expo-router/stack";
-import { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { ActivityIndicator } from "react-native-paper";
+import { ReducedMotionConfig, ReduceMotion } from "react-native-reanimated";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
-export default function Layout() {
-  useEffect(() => {
-    NavigationBar.setPositionAsync("absolute");
-    NavigationBar.setBackgroundColorAsync("#00000000");
-  }, []);
+NavigationBar.setPositionAsync("absolute");
+NavigationBar.setBackgroundColorAsync("#00000000");
 
+export default function Layout() {
   return (
     <Provider store={store}>
       <PersistGate loading={<ActivityIndicator />} persistor={persistor}>
@@ -22,13 +21,16 @@ export default function Layout() {
             icon: (props: any) => <Ionicons {...props} />,
           }}
         >
-          <Stack
-            screenOptions={{
-              animation: "default",
-              headerShown: false,
-            }}
-            initialRouteName="/"
-          />
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <ReducedMotionConfig mode={ReduceMotion.Never} />
+            <Stack
+              screenOptions={{
+                animation: "default",
+                headerShown: false,
+              }}
+              initialRouteName="/"
+            />
+          </GestureHandlerRootView>
         </Material3ThemeProvider>
       </PersistGate>
     </Provider>
